b0d93b40c28ab0a02f0fe186d501d4694d0b6043
Tatianaaa-Lpe <Tatianaaa.lpe@gmail.com>
2025-04-10 20:34:35 +0200

    Update Activation_Pare-feu_Linux.sh

--- Scripts/Activation_Pare-feu_Linux.sh
+++ Scripts/Activation_Pare-feu_Linux.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
 #Vérifier le status du pare-feu
-<<<<<<< HEAD
 echo "vérification du status du pare-feu"
-=======
-echo "Vérification status du pare-feu"
->>>>>>> af12c52591daf751b37ea580aeea2940bcb024c5
 sudo ufw status
 
 #Activation du pare-feu
