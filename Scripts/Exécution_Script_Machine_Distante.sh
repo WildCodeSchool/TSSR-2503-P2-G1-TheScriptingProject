c0c7af6be39a24599a7402cfbc8474eb12d75867
tatianaaa-lpe <tatianaaa.lpe@gmail.com>
2025-04-16 15:37:18 +0200

    Ajout

--- Scripts/Exécution_Script_Machine_Distante.sh
+++ Scripts/Exécution_Script_Machine_Distante.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
+#Demande nom utilisateur
+echo "Quel utilisateur souhaitez-vous connecté?"
+read user
+
+#Demande nom de la machine
+echo "Sur quelle machine souhaitez-vous être connectée?"
+read computer
 
 #Connection SSH
-#Connection à la machine distante 
+ssh "$user"@"$computer"
+
 #Demande nom du script
+echo "Quel script souhaitez-vous exécuter? (Noter le chemin complet jusqu'au script)"
+read nameScript
+
 #Exécution du script 
+if [ -e $nameScript ]
+then 
+    ./$nameScript
+else
+    echo "Le script n'existe pas"
+    exit 1
+fi
+exit 0
