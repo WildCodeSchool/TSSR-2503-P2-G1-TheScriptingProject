efbce247034f5104e5e9aecef22d79d6dd8b893e
tatianaaa-lpe <tatianaaa.lpe@gmail.com>
2025-04-18 01:10:40 +0200

    Ajout

--- Script_PowerShell/Creation_repertoire.ps1
+++ Script_PowerShell/Creation_repertoire.ps1
@@ -1,4 +1,5 @@
-#DEmander le nom du dossier à créer
+#Demander le nom du dossier à créer
 $nomDossier = Read-Host "Entrez le nom du dossier"
 $chemin = Read-Host "Entrez le chemin complet"
 New-Item -ItemType Directory -Name $nomDossier -Path $chemin
+Write-Host "Répertoire créé" -ForegroundColor Green
