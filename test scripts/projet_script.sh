fb61494edd80b02af291ec494e6cad9b19d9af26
John <thehorusgaze@gmail.com>
2025-04-17 10:21:33 +0200

    mise à jour finale

--- test scripts/projet_script.sh
+++ test scripts/projet_script.sh
@@ -3,13 +3,185 @@
 #----------------------------------------------------------------------------------------------------------------------
 #				Fonctions utilisées ACTIONS UTILISATEUR
 #----------------------------------------------------------------------------------------------------------------------
+#Fonction de création de compte utilisateur local
+function creating_a_local_user_account()
+{
+    #Demande Nom du nouvel utilisateur à créer
+    echo " Quel est le nom du nouvel utilisateur?"
+    read new_user
+
+    #Vérifier si l'utilisateur existe déjà
+    if cat /etc/passwd | grep $new_user > /dev/null
+    then
+        echo "L'utilisateur existe déjà"
+        exit 1
+    else
+        echo "Création du nouvel utilisateur"
+        sudo useradd $new_user > /dev/null
+            #Vérifier si l'utilisateur a bien été créer
+            if cat /etc/passwd | grep $new_user > /dev/null
+            then
+                echo "L'utilisateur a été créer"
+            else
+                echo "L'utilisateur n'a pas été crée = ERREUR"
+            fi
+    fi
+exit 0
+}
 
 
+#Fonction Changement de mot de passe
+function change_password ()
+{
+    #Changement du mot de passe
+    echo "Entrer le nom de l'utilisateur"
+    read nom_utilisateur
+    echo "Entrer le nouveau mot de passe"
+    read -s mot_de_passe1
+    echo "Entrer de nouveau le mot de passe"
+    read -s mot_de_passe2
+
+    #Vérifier que les mots de passe sont identiques
+    if [$mot_de_passe1 != $mot_de_passe2]
+    then
+        echo "Les mots de passe ne sont pas identiques"
+    fi
+exit 0
+}
 
 
+#Fonction de la suppression de compte utilisateur local
+function deleting_a_local_user_account ()
+{
+    #Donner le nom de l'utilisateur à supprimer
+    echo "Quel est le nom de l'utilisateur à supprimer?"
+    read user_name
+
+    #Vérifier l'existence de l'utilisateur
+    if cat /etc/passwd | grep $user_name > /dev/null
+    then
+        echo "Suppression de l'utilisateur en cours"
+        sudo userdel $user_name
+
+        #Vérifier la suppression de l'utilisateur
+        if cat etc/passwd | grep $user_name > /dev/null
+        then
+            echo "ERREUR utilisateur non supprimé"
+        else
+            echo "L'utilisateur a bien été supprimer"
+        fi
+    else
+        echo "L'utilisateur n'existe pas"
+        echo "Aucun utilisateur supprimé"
+        exit 1
+    fi
+exit 0
+}
 
 
+#Fonction de la désactivation de compte utilisateur local
+function local_user_account_deactivation ()
+{
+    #Donner le nom de l'utilisateur à Désactiver
+    echo "Donner le nom utilisateur à désacitver"
+    read $name_user
 
+    #Vérifier si l'utilisateur existe
+    if cat /etc/passwd | grep "$name_user" > /dev/null
+    then
+        #Désactiver l'utilisateur
+        echo "Désactivation de l'utilisateur"
+        sudo usermod -L
+        echo "Utilisateur désactivé"
+    else
+        #Problème de désactivation
+        echo "Le nom utilisateur n'existe pas"
+        exit 1
+    fi
+exit 0
+}
+
+
+#Fonction d'ajout à un groupe d'administration
+function adding_to_an_administration_group ()
+{
+    #Demander le nom utilisateur
+    echo "Quel utilisateur souhaitez-vous ajouter au groupe administrateur?"
+    read user_name
+
+    #Vérifier si l'utilisateur existe
+    if cat /etc/passwd | grep $user_name > /dev/null
+    then
+        #Ajouter l'utisateur au groupe administrateur
+        sudo usermod -aG adm $user_name
+        #Vérifier si l'utilisateur est bien dans le groupe demandé
+        if groups $user_name
+        then
+            echo "L'utilisateur a bien été ajouté"
+        fi
+    else
+        echo "L'utilisateur n'existe pas"
+        exit 1
+    fi
+exit 0
+}
+
+
+#Fonction d'ajout à un groupe local
+function adding_to_a_local_group ()
+{
+    #Demander le nom utilisateur
+    echo "Quel utilisateur souhaitez-vous ajouter à un groupe?"
+    read user_name
+
+    #Vérifier si l'utilisateur existe
+    if cat /etc/passwd | grep $user_name > /dev/null
+    then
+        #Demander le nom du groupe
+        echo "Dans quel groupe souhaitez-vous l'ajouter?"
+        read name_group
+        #Ajouter l'utisateur au groupe
+        sudo adduser $user_name $name_group
+        #Vérifier si l'utilisateur est bien dans le groupe demandé
+        if groups $user_name
+        then
+            echo "L'utilisateur a bien été ajouté"
+        fi
+    else
+        echo "L'utilisateur n'existe pas"
+        exit 1
+    fi
+exit 0
+}
+
+
+#Fonction de sortie d’un groupe local
+function local_band_release ()
+{
+    #Demander le nom utilisateur
+    echo "Quel utilisateur souhaitez-vous supprimer d'un groupe?"
+    read user_name
+
+    #Vérifier si l'utilisateur existe
+    if cat /etc/passwd | grep $user_name > /dev/null
+    then
+        #Demander le nom du groupe
+        echo "Dans quel groupe souhaitez-vous le supprimer?"
+        read name_group
+        #Suppression de l'utisateur du groupe
+        sudo gpasswd -d $user_name $name_group
+
+        #Vérifier si l'utilisateur est bien dans le groupe demandé
+        if groups $user_name
+        then
+            echo "L'utilisateur a bien été supprimé"
+        fi
+    else
+        echo "L'utilisateur n'existe pas"
+        exit 1
+    fi
+exit 0
+}
 
 
 #----------------------------------------------------------------------------------------------------------------------
@@ -123,7 +295,7 @@ function access_folder()
     if [ ! -d "$folder" ]
     then
 	read -p "le dossier n'existe pas, appuyer sur la touche [Entrée] pour continuer"
-    	access_folder
+        access_folder
     else
         echo "Voici les droits/permissions de l'utilisateur sur ce dossier :"
         ls -ld $folder
@@ -155,7 +327,7 @@ function access_file()
     else
         echo "Voici les droits/permissions de l'utilisateur sur ce fichier :"
         ls -l $file
-    	read -p "vous voulez continuer ? [Oui/Non]" choix
+        read -p "vous voulez continuer ? [Oui/Non]" choix
     case $choix in
     Oui)
         info_user_menu_choice ;;
@@ -171,7 +343,268 @@ function access_file()
 #------------------------------------------------------------------------------------------------------------------
 #				 Fonctions utilisées ACTIONS ORDINATEUR
 #------------------------------------------------------------------------------------------------------------------
+#Fonction Arrêt ordinateur
+function computer_shutdown ()
+{
+    #Arrêt Machine
+    echo "Confirmation arrêt machine (yes/no)"
+    #Confirmation ou non de l'arrêt
+    read choix
 
+    case $choix in
+        yes)
+            echo "Confirmation arrêt machine"
+            echo "Arrêt en cours"
+            poweroff ;;
+        no)
+            echo "Annulation arrêt de la machine"
+            echo "Pas d'arrêt" ;;
+    esac
+exit 0
+}
+
+
+#Fonction de redémarrage de l'ordinateur
+function restart_computer ()
+{
+    #Redémarrage de la machine
+    echo "Confirmation du redémarrage (yes/no)"
+    #Lecture du choix de redémarrage
+    read choix
+    case $choix in
+        yes)
+            echo "Confirmation du redémarrage"
+            echo "Redémarrage de la machine en cours"
+            reboot ;;
+        no)
+            echo "Annulation du redémarrage"
+            echo "Pas de redémarrage de la machine" ;;
+    esac
+exit 0
+}
+
+
+#Fonction de verrouillage #Redémarrage de la machine
+function computer_lock ()
+{
+    #Verrouillage de la machine
+    echo "Confirmation du verrouillage (yes/no)"
+    #Lecture du choix de verrouillage
+    read choix
+    case $choix in
+        yes)
+            echo "Confirmation du verrouillage"
+            echo "Verrouillage en cours"
+            usermod -L ;;
+        no)
+            echo "Annulation du verrouillage"
+            echo "Pas de verrouillage" ;;
+    esac
+exit 0
+}
+
+
+#Fonction de mise-à-jour du système
+function system_update ()
+{
+    #Mise à jour du système
+    echo "voulez-vous mettre à jour le système? (yes/no)"
+
+    #Lecture choix de la mise à jour
+    read choix
+    case $choix in
+        yes)
+            echo "Mise à jour du système confirmée"
+            echo "Mise à jour du systèeme en cours"
+            sudo apt update ;;
+        no)
+            echo "Mise à jour du système avorté"
+            echo "Pas de mise à jour" ;;
+    esac
+exit 0
+}
+
+
+#Fonction de création de répertoire
+function directory_creation ()
+{
+    nom_du_repertoire=$nom_du_repertoire
+
+    #Création d'un répertoire
+    echo "Donner le nom du répertoire à créer"
+    read nom_du_repertoire
+    mkdir $nom_du_repertoire
+
+    #Vérification de l'existence d'un répertoire du même nom
+    if [ $? = 0 ]
+    then
+        echo "Le répertoire a été créer"
+    else
+        echo "Le répertoire existe déjà"
+    fi
+exit 0
+}
+
+
+#Fonction de modification de répertoire
+function directory_modification ()
+{
+    #Nom du répertoire
+    echo "Quel est le nom du répertoire à modifier?"
+    read old_name
+    #Nouveau nom du répertoire
+    read new_name
+
+    #Vérifier si le nouveau nom n'existe pas
+    if [ -d "$new_name" ]
+    then
+        echo "Le nom du répertoire existe déjà"
+    else
+        mv "$old_name" "$new_name"
+        echo "La modification du nom a bien été réaliser"
+    fi
+exit 0
+}
+
+
+
+#Fonctione de suppression de répertoire
+function directory_deletion ()
+{
+    #Récupérer le nom du répertoire à supprimer
+    echo "Quel est le répertoire à supprimer?"
+    read nom_du_repertoire
+
+    #Vérifier si le répertoire existe
+    if [ -e $nom_du_repertoire ]
+    then
+        rm -r $nom_du_repertoire
+        echo "Le répertoire a été supprimer"
+    else
+        echo "Le répertoire n'existe pas"
+    fi
+exit 0
+}
+
+
+#Fonction de prise de main à distance (CLI)
+function remote_control ()
+{
+    #Demande utilisateur
+    echo "Avec quel utilisateur souhaitez-vous être connecté?"
+    read user
+
+    #Demande sur quelle machine se connecter
+    echo "Sur quelle machine souhaitez-vous être connectée?"
+    read computer
+
+    #Connection SSH
+    ssh "$user"@"$computer"
+
+exit 0
+}
+
+
+#Fonction d'activation du pare-feu
+function enabling_the_firewall ()
+{
+    #Vérifier le status du pare-feu
+    echo "Vérification status du pare-feu"
+    sudo ufw status
+
+    #Activation du pare-feu
+    echo "Voulez-vous activer le pare-feu? (yes/no)"
+    read choix
+
+    case $choix in
+    yes)
+        echo "Activation du pare-feu en cours"
+        sudo ufw enable
+        echo "Pare-feu activé" ;;
+    no)
+        echo "Aucun pare-feu activé" ;;
+    esac
+exit 0
+}
+
+
+#Fonction de désactivation du pare-feu
+function disabling_the_firewall ()
+{
+    #Vérifier le status du pare-feu
+    echo "Vérification status du pare-feu"
+    sudo ufw status
+
+    #Désactivation du pare-feu
+    echo "Voulez-vous désactiver le pare-feu? (yes/no)"
+    read choix
+
+    case $choix in
+    yes)
+        echo "Désactivation du pare-feu en cours"
+        sudo ufw disable
+        echo "Pare-feu désactivé" ;;
+    no)
+        echo "Aucun pare-feu désactivé" ;;
+    esac
+exit 0
+}
+
+
+#Fonction d'installation de logiciel
+function software_installation ()
+{
+    #Demander le nom du logiiciel à installer
+    echo "Quel logiciel souhaitez-vous installer?"
+    read app
+
+    #Installation du logiciel
+    echo "Installation du logiciel en cours"
+    sudo apt install $app
+exit 0
+}
+
+
+#Fonction de désinstallation de logiciel
+function_software_uninstallation ()
+{
+    #Demander quel logiciel est à désinstaller
+    echo "Quel logiciel souhaitez-vous désinstaller?"
+    read app
+
+    #Lancer la désinstallation
+    echo "Désinstallation du logiciel"
+    sudo apt purge $app
+exit 0
+}
+
+
+#Fonction d'exécution de script sur la machine distante
+function script_execution_on_remote_machine ()
+{
+    #Demande nom utilisateur
+    echo "Quel utilisateur souhaitez-vous connecté?"
+    read user
+    #Demande nom de la machine
+    echo "Sur quelle machine souhaitez-vous être connectée?"
+    read computer
+    #Connection SSH
+    ssh "$user"@"$computer"
+
+    #Demande nom du script
+    echo "Quel script souhaitez-vous exécuter? (Noter le chemin complet jusqu'au script)"
+    read name_script
+
+    #Exécution du script
+    if [ -e $name_script ]
+    then
+        ./$name_script
+    else
+        echo "Le script n'existe pas"
+        exit 1
+    fi
+exit 0
+}
 
 
 #-----------------------------------------------------------------------------------------------------------------
@@ -261,12 +694,12 @@ function info_folder()
 	cd ~/
 	read -p "Entrez le nom d'un dossier sur lequel vous voulez des info ? " dossier
 	sudo find -type d -name "$dossier"
-     if [ ! -d "$dossier" ]
+        if [ ! -d "$dossier" ]
 	then
 	read -p "le dossier n'existe pas, appuyer sur la touche [Entrée] pour continuer"
 	info_folder
 	else
-        	echo "Voici le nom et l'espace disque du dossier :"
+        echo "Voici le nom et l'espace disque du dossier :"
 	du -sh ~/$dossier
     read -p "vous voulez continuer ? [Oui/Non]" choix
     case $choix in
@@ -420,7 +853,7 @@ function user_event ()
 #-----------------------------------------------------------------------------------------------------------------------------------------
 
 #Fonction du sous Menu des actions utilisateur
-action_user_menu_choice ()
+function action_user_menu_choice ()
 {
 	clear
         echo "===== Menu des actions utilisateur ====="
@@ -438,19 +871,19 @@ action_user_menu_choice ()
 
 	case $action_user_menu_choice in
 	1)
-		action 1;;
+		creating_a_local_user_account;;
 	2)
-		action 2;;
+		change_password;;
 	3)
-		action 3;;
+		deleting_a_local_user_account;;
 	4)
-		action 4;;
+		local_user_account_deactivation;;
 	5)
-		action 5;;
+		adding_to_an_administration_group;;
 	6)
-		action 6;;
+		adding_to_a_local_group;;
 	7)
-		action 7;;
+		local_band_release;;
 	8)
 		user_menu;;
 	*)
@@ -461,7 +894,7 @@ action_user_menu_choice ()
 
 
 #Fonction du sous Menu des informations utilisateur
-info_user_menu_choice ()
+function info_user_menu_choice ()
 {
 	clear
         echo "===== Menu des informations utilisateur ====="
@@ -478,19 +911,19 @@ info_user_menu_choice ()
         read -p "Choisissez une information " info_user_menu_choice
 
 	case $info_user_menu_choice in
- 	1)
+	1)
         	lastdate_connexion;;
- 	2)
+	2)
         	lastdate_passwd;;
- 	3)
+	3)
         	open_user_session;;
- 	4)
+	4)
         	user_groups;;
- 	5)
+    	5)
         	allcommand_user;;
- 	6)
+    	6)
         	access_folder;;
- 	7)
+	7)
         	access_file;;
 	8)
 		user_menu;;
@@ -502,14 +935,14 @@ info_user_menu_choice ()
 
 #Fonction du sous Menu des actions Ordinateur
 
-action_computer_menu_choice ()
+function action_computer_menu_choice ()
 {
         clear
         echo "===== Menu des actions ordinateur ====="
         echo "                                       "
-        echo "1. Arrêt"
-        echo "2. Redémarrage"
-        echo "3. Verrouillage"
+        echo "1. Arrêt ordinateur"
+        echo "2. Redémarrage ordinateur"
+        echo "3. Verrouillage ordinateur"
         echo "4. Mise-à-jour du système"
         echo "5. Création de répertoire"
         echo "6. Modification de répertoire"
@@ -526,31 +959,31 @@ action_computer_menu_choice ()
 
 	case $action_computer_menu_choice in
 	1)
-	;;
+        	computer_shutdown;;
 	2)
-	;;
+        	restart_computer;;
 	3)
-	;;
+        	computer_lock;;
 	4)
-	;;
+        	system_update;;
 	5)
-	;;
+        	directory_creation;;
 	6)
-	;;
+        	directory_modification;;
 	7)
-	;;
+        	directory_deletion;;
 	8)
-	;;
+        	remote_control;;
 	9)
-	;;
+        	enabling_the_firewall;;
 	10)
-	;;
+        	disabling_the_firewall;;
 	11)
-	;;
+        	software_installation;;
 	12)
-	;;
+        	software_uninstallation;;
 	13)
-	;;
+        	script_execution_on_the_remote_machine;;
 	14)
 		computer_menu;;
 	*)
@@ -564,7 +997,7 @@ action_computer_menu_choice ()
 
 #Fonction du sous Menu des informations Ordinateur
 
-info_computer_menu_choice ()
+function info_computer_menu_choice ()
 {
 	clear
         echo "===== Menu des informations ordinateur ====="
@@ -586,7 +1019,7 @@ info_computer_menu_choice ()
 
 	case $info_computer_menu_choice in
 	1)
-        	os_version;;
+    		os_version;;
 	2)
 		disk_number;;
 	3)
@@ -596,17 +1029,17 @@ info_computer_menu_choice ()
 	5)
 		info_folder;;
 	6)
-       		mounted_drive_list;;
+        	mounted_drive_list;;
 	7)
-       		pack_app_installed;;
+        	pack_app_installed;;
 	8)
-       		serv_progress;;
+        	serv_progress;;
 	9)
-       		local_users_list;;
+        	local_users_list;;
 	10)
-       		total_ram;;
+        	total_ram;;
 	11)
-       		ram_used;;
+        	ram_used;;
 	12)
 		computer_menu;;
 	*)
@@ -621,7 +1054,7 @@ info_computer_menu_choice ()
 #------------------------------------------------------------------------------------------------------------------------------
 
 #Fonction du Menu Utilisateur
-user_menu ()
+function user_menu ()
 {
 	clear
         echo "===== Menu Utilisateur ====="
@@ -635,12 +1068,12 @@ user_menu ()
 	case $user_menu_choice in
 	# Menu Utilisateur Action
 	1)
-	        action_user_menu_choice;;
+        	action_user_menu_choice;;
 	#Menu Utilisateur Information
 	2)
         	info_user_menu_choice;;
 	3)
-	main_menu;;
+		main_menu;;
 	*)
         read -p "Choix invalide, appuyer sur la touche [Entrée] pour continuer"
         user_menu;;
@@ -649,7 +1082,7 @@ user_menu ()
 
 
 #Fonction du Menu Ordinateur
-computer_menu ()
+function computer_menu ()
 {
 	clear
         echo "===== Menu Ordinateur ====="
@@ -667,7 +1100,7 @@ computer_menu ()
 	2)
 		info_computer_menu_choice;;
 	3)
-	main_menu;;
+		main_menu;;
 	*)
         read -p "Choix invalide, appuyer sur la touche [Entrée] pour continuer"
         computer_menu;;
@@ -677,7 +1110,7 @@ computer_menu ()
 
 
 #Fonction du Menu Script
-script_menu ()
+function script_menu ()
 {
 	clear
 	echo "===== Menu Script ====="
@@ -695,7 +1128,7 @@ script_menu ()
 	2)
 		computer_event;;
 	3)
-	main_menu;;
+		main_menu;;
 	*)
 	read -p "Choix invalide, appuyer sur la touche [Entrée] pour continuer"
         script_menu;;
@@ -721,7 +1154,7 @@ function main_menu ()
 	1)
 		user_menu;;
 	2)
-      		computer_menu;;
+        	computer_menu;;
 	3)
 		script_menu;;
 	*)
