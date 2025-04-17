0985ef894941bc7f86231039c8b987e22d1f58f5
tatianaaa-lpe <tatianaaa.lpe@gmail.com>
2025-04-17 10:41:24 +0200

    Modif script

--- Scripts/Changement_mot_de_passe.sh
+++ Scripts/Changement_mot_de_passe.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 
 #Changement du mot de passe
-
 echo "Entrer le nom de l'utilisateur"
-read nomUtilisateur
-echo "Entrer le nouveau mot de passe"
-read -s motDePasse1
-echo "Entrer de nouveau le mot de passe"
-read -s motDePasse2
+read -p nom_utilisateur
 
-#Vérifier que les mots de passe sont identiques
-
-if [$motDePasse1 != $motDePasse2]
-then
-    echo "Les mots de passe ne sont pas identiques"
-fi 
+#Enregistrement du mot de passe
+sudo passwd $nom_utilisateur 
 exit 0
